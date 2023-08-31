47b221bb992263ddfa73eb505f5736cfc6bd24dc
myreallycoolusername <91684986+myreallycoolusername@users.noreply.github.com>
2023-08-31 20:34:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -173,5 +173,5 @@ async def generate():
 
 
 # Run app on port 5000 (default)
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=3000)
+#if __name__ == "__main__":
+ # app.run(host="0.0.0.0", port=3000)
