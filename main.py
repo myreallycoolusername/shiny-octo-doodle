e48d7de6746d8e37b779395ab4abb51abd7ad1df
myreallycoolusername <91684986+myreallycoolusername@users.noreply.github.com>
2023-11-08 14:03:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ def transcript():
         {"role": "user", "content": wholesearchsys}
     ]
     proxy=os.getenv('PROXY2'),
-    thingtosearch = g4f.ChatCompletion.create(model='13B', provider=g4f.Provider.Llama2, messages=messages1)
+    thingtosearch = g4f.ChatCompletion.create(model=g4f.models.default, provider=g4f.Provider.Llama2, messages=messages1)
     if internet == "on":
         async def search2():
             with AsyncDDGS(proxies=os.getenv('PROXY'), timeout=120) as ddgs:
